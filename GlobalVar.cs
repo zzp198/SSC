ee0b45edfef847e9fc12c5af8b1f10a591d361fa
zzp198 <zzp198@163.com>
2023-06-07 18:43:32 +0800

    rebuild project

---
+++ GlobalVar.cs
@@ -0,0 +1,16 @@
+﻿namespace SSC;
+
+internal static class GlobalVar
+{
+    internal static bool SSCisRunning;
+
+    static GlobalVar()
+    {
+        Init();
+    }
+
+    static void Init()
+    {
+        SSCisRunning = false;
+    }
+}
---
+++ PingPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SSC;
+
+public class PingPlayer : ModPlayer
+{
+    
+}
