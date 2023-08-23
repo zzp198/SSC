b12a9deda1e548a66735ba6c2c28982f67ea6912
zzp198 <zzp198@163.com>
2023-08-23 23:17:31 +0800

    update message system

--- Core/MessageManager.cs
+++ Core/MessageManager.cs
@@ -1,8 +1,23 @@
-﻿using Terraria.ModLoader;
+﻿using System.Collections.Generic;
+using Terraria.ModLoader;
 
 namespace SSC.Core;
 
 public class MessageManager : ModSystem
 {
-    
+    public List<(byte[] data, string hash)> MessageSegment;
+
+    public override void OnModLoad()
+    {
+        MessageSegment = new List<(byte[] data, string hash)>(255);
+    }
+
+    public void SendMessage(ModPacket mp, int to, int from)
+    {
+    }
+
+    public override void OnModUnload()
+    {
+        MessageSegment = null;
+    }
 }
